ebe22d0c906601669515a7e705c5939409189056
Unknown <delorme.tanguy@gmail.com>
2018-11-09 08:15:34 +0100

    install project

---
+++ careeat/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Début du projet de recherche et innovation pour CareEat</Text>
+        <Text>Developped by HE Chen, STER Alexis and DELORME Tanguy</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
