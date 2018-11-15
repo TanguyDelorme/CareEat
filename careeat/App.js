c01d3bbac80f4e7cc5fff03d9f064bcc901deff9
Unknown <delorme.tanguy@gmail.com>
2018-11-15 11:48:03 +0100

    menu app

--- careeat/App.js
+++ careeat/App.js
@@ -1,23 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default class App extends React.Component {
+class FindRestaurants extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Début du projet de recherche et innovation pour CareEat</Text>
-        <Text>Developped by HE Chen, STER Alexis and DELORME Tanguy</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Find restaurants around you!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class ScanDocument extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Scan your documents!</Text>
+      </View>
+    );
+  }
+}
+
+class MyCart extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Check your cart!</Text>
+      </View>
+    );
+  }
+}
+
+export default createBottomTabNavigator(
+  {
+    Restaurants:
+    {
+        screen: FindRestaurants,
+        navigationOptions: () => ({
+           tabBarIcon: ({tintColor}) => (
+              <Ionicons  name='ios-compass' color={tintColor} size={24}/>
+           ),
+         }),
+     },
+    Scan:
+    {
+        screen: ScanDocument,
+        navigationOptions: () => ({
+           tabBarIcon: ({tintColor}) => (
+               <Ionicons  name='ios-qr-scanner' color={tintColor} size={24}/>
+           ),
+        }),
+         },
+    Cart:
+    {
+        screen: MyCart ,
+        navigationOptions: () => ({
+           tabBarIcon: ({tintColor}) => (
+              <Ionicons  name='ios-cart  ' color={tintColor} size={24}/>
+            ),
+         }),
+    },
   },
-});
+
+);
